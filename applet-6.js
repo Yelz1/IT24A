fe5a05b227bbd3aab846072942b9b300d9815b63
Yelz1 <Yelz1>
2024-10-16 13:30:33 +0800

    Added textinput

--- applet-6.js
+++ applet-6.js
@@ -2,6 +2,11 @@ class WeatherApp {
     constructor(apiKey) {
         this.apiKey = apiKey;
 
-        
+        this.cityInput = document.getElementById('cityInput');
+        this.getWeatherBtn = document.getElementById('getWeatherBtn');
+
+       
+        this.getLocationBtn = document.getElementById('getLocationBtn');  
+
     }
 }
